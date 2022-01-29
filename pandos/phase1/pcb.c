24c16fc9399779b2b7808ea55a351ddce9a7eb6a
andreaiaia <andreaiaia@hotmail.it>
2022-01-29 17:45:39 +0100

    creati i file su cui lavorare per la fase 1

---
+++ pandos/phase1/pcb.c
@@ -0,0 +1,5 @@
+#include "pcb.h"
+
+// TODO: Lista pcbFree, da inizializzare con initPcbs()
+// Per maggiori info pag. 19 di pandos.pdf
+
---
+++ pandos/phase1/pcb.h
@@ -0,0 +1,35 @@
+#include "../h/pandos_const.h"
+#include "../h/pandos_types.h"
+
+// Allocation and deallocation of pcbs
+
+/* I pcb non più usati vengono restituiti alla lista pcbFree */
+void freePcb(pcb_t *p);
+
+
+// Process Queue Maintenance
+
+/* 
+Questo metodo serve per inizializzare una variabile per fare da puntatore di coda per una process queue.
+Return: pointer to the tail of an empty process queue
+        or NULL.
+*/
+pcb_t *mkEmptyProcQ();
+
+/* 
+Rimuove il primo elemento il cui puntatore a coda è puntato da tp dalla process queue.
+Se necessario aggiorna il puntatore di coda della process queue.
+Return: NULL if the process queue is empty
+        or pointer to removed element.
+*/
+pcb_t *removeProcQ(pcb_t **tp);
+
+
+// Process Tree Maintenance
+
+/*
+Prende il primo child del pcb puntato da p non più un figlio di p.
+Return: NULL if initially there where no children of p
+        or pointer to the removed child.
+*/
+pcb_t *removeChild(pcb_t *p);
