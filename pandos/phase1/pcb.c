d319e786fe496b8576e63d3f77d52d58d9d3edc2
alexlorenzato <alexlorenzato@gmail.com>
2022-01-30 19:00:35 +0100

    initPcbs() implementata

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -5,3 +5,16 @@
 LIST_HEAD(pcbFree_h)
 
 // Per maggiori info pag. 19 di pandos.pdf
+
+
+// CODICE SCRITTO DA QUEL POVERACCIO DI ALEX //
+pcb_t pcbFree_table[MAX_PROC];
+/* Inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table. 
+Questo metodo deve essere chiamato una volta sola in fase di inizializzazione della struttura dati. */
+void initPcbs(){
+   if(list_empty(&(pcbFree_h)){ //non sono sicuro di questo if
+		for(int i=0; i<MAX_PROC; i++){
+	        list_add( &(pcbFree_table[i].p_next), &(pcbFree_h) );
+		}
+   }  
+}       
--- pandos/phase1/pcb.h
+++ pandos/phase1/pcb.h
@@ -2,31 +2,41 @@
 #include "../h/pandos_types.h"
 #include "../h/listx.h"
 
-// Allocation and deallocation of pcbs
 
-/* I pcb non più usati vengono restituiti alla lista pcbFree_h */
+
+// Allocation and deallocation of pcbs //
+
+/* 
+	1. Inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table. 
+	Questo metodo deve essere chiamato una volta sola in fase di inizializzazione della struttura dati. 
+*/
+void initPcbs();
+
+/* 
+	3. I pcb non più usati vengono restituiti alla lista pcbFree_h 
+*/
 void freePcb(pcb_t *p);
 
 
-// Process Queue Maintenance
+// Process Queue Maintenance //
 
 /* 
-Questo metodo serve per inizializzare una variabile per fare da puntatore di coda per una process queue.
-Return: pointer to the tail of an empty process queue
-        or NULL.
+	Questo metodo serve per inizializzare una variabile per fare da puntatore di coda per una process queue.
+	Return: pointer to the tail of an empty process queue
+            or NULL.
 */
 pcb_t *mkEmptyProcQ();
 
 /* 
-Rimuove il primo elemento il cui puntatore a coda è puntato da tp dalla process queue.
-Se necessario aggiorna il puntatore di coda della process queue.
-Return: NULL if the process queue is empty
-        or pointer to removed element.
+	Rimuove il primo elemento il cui puntatore a coda è puntato da tp dalla process queue.
+	Se necessario aggiorna il puntatore di coda della process queue.
+	Return: NULL if the process queue is empty 
+		    or pointer to removed element.
 */
 pcb_t *removeProcQ(pcb_t **tp);
 
 
-// Process Tree Maintenance
+// Process Tree Maintenance //
 
 /*
 Prende il primo child del pcb puntato da p non più un figlio di p.
