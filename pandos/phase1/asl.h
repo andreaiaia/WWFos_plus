f529ac7a00e586e8132ea93eb43396f8ce9832ca
Andrea Bianchi <bianan96@gmail.com>
2022-02-03 09:54:46 +0100

    aggiunti commenti alle funzioni

--- pandos/phase1/asl.h
+++ pandos/phase1/asl.h
@@ -2,12 +2,28 @@
 #include "../h/pandos_types.h"
 #include "../h/listx.h"
 
-// Tabella dei semafori di dimensione massima MAXPROC
-semd_t semd_table[MAXPROC];
 
-// Lista dei semafori liberi/inutilizzati
-LIST_HEAD(semdFree_h);
+// Gestione della ASL (Active Semaphore List)
 
-// Lista dei semafori attivi/utilizzati
-LIST_HEAD(semd_h);
+/*
+    16. Rimuove il PCB puntato da p dalla coda del semaforo su cui è bloccato (p->p_semAdd).
+    Se la coda dei processi bloccati per il semaforo diventa vuota, rimuove il semd corrispondente da semd_h e lo inserisce in semdFree_h (lo rimette nella coda dei semafori liberi).
+    @param: puntatore a PCB da rimuovere.
+    Return: p, puntatore al PCB rimosso
+            or NULL se il PCB non compare nella coda (stato di errore).
+*/
+pcb_t *outBlocked(pcb_t *p);
 
+/*
+    17. Restituisce (senza rimuoverlo) il puntatore al primo PCB della coda dei processi associata al SEMD con chiave semAdd.
+    @param: chiave del SEMD.
+    Return: puntatore al primo pcb_t nella coda dei processi
+            or NULL se non c'è il SEMD o se la sua coda è vuota.
+*/
+pcb_t *headBlocked(int *semAdd);
+
+/*
+    18. Inizializza la lista semdFree in modo da contenere tutti gli elementi della semdTable.
+    Questo metodo viene invocato una volta sola durante l'inizializzazione dei dati.
+*/
+void initASL();
