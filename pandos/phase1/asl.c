f9d0cd308e9263d528d2debcba042daa4cc06967
Wifino <niccolo.cicioni@gmail.com>
2022-02-05 16:51:31 +0100

    Inizio funzione 14

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -1,5 +1,27 @@
 #include "asl.h"
 
+/*
+    14. Viene inserito il PCB puntato da p nella coda dei
+    processi bloccati associata al SEMD con chiave
+    semAdd. Se il semaforo corrispondente non è
+    presente nella ASL, alloca un nuovo SEMD dalla
+    lista di quelli liberi (semdFree) e lo inserisce nella
+    ASL, settando I campi in maniera opportuna (i.e.
+    key e s_procQ). Se non è possibile allocare un
+    nuovo SEMD perché la lista di quelli liberi è vuota,
+    restituisce TRUE. In tutti gli altri casi, restituisce
+    FALSE
+
+*/
+
+int insertBlocked(int *semAdd, pcb_t *p){
+
+
+
+
+
+}
+
 
 /*
     15. Rimuove il primo PCB dalla coda dei processi bloccati (s_procq) associata al SEMD della ASL con chiave semAdd. Se la coda dei processi bloccati per il semaforo diventa vuota, rimuove il descrittore corrispondente dalla ASL e lo inserisce nella coda dei descrittori liberi (semdFree_h). 
--- pandos/phase1/asl.h
+++ pandos/phase1/asl.h
@@ -14,6 +14,23 @@ LIST_HEAD(semd_h);
 
 // Gestione della ASL (Active Semaphore List)
 
+
+/*
+    14. Viene inserito il PCB puntato da p nella coda dei
+    processi bloccati associata al SEMD con chiave
+    semAdd. Se il semaforo corrispondente non è
+    presente nella ASL, alloca un nuovo SEMD dalla
+    lista di quelli liberi (semdFree) e lo inserisce nella
+    ASL, settando I campi in maniera opportuna (i.e.
+    key e s_procQ). Se non è possibile allocare un
+    nuovo SEMD perché la lista di quelli liberi è vuota,
+    restituisce TRUE. In tutti gli altri casi, restituisce
+    FALSE
+
+*/
+
+int insertBlocked(int *semAdd, pcb_t *p);
+
 /*
     15. Ritorna il primo PCB dalla coda dei processi bloccati (s_procq) associata al SEMD della ASL con chiave semAdd. 
     Se tale descrittore non esiste nella ASL, restituisce NULL. Altrimenti, restituisce l’elemento rimosso. 
