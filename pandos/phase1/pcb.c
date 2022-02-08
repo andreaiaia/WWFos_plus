2e64ee08862edb51ee8549e91b451103363ee602
Andrea Bianchi <bianan96@gmail.com>
2022-02-08 23:10:07 +0100

    fix mkEmptyProcQ

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -68,7 +68,8 @@ pcb_t *allocPcb() {
 	4. Questa funzione prende il puntatore passatogli e usa la macro del kernel linux per creare una lista di PCB vuota.
 */
 void mkEmptyProcQ(struct list_head * head) {
-	head = &(LIST_HEAD(procQ));
+	LIST_HEAD(procQ);
+	head = &procQ;
 }
 
 /* 
