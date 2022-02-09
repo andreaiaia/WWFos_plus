b0fd4d774d5b83ffa6c503a681b0ff4243bab5c3
Wifino <niccolo.cicioni@gmail.com>
2022-02-09 16:08:48 +0100

    Update pcb.c

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -110,8 +110,9 @@ pcb_t *headProcQ(struct list_head *head) {
 */
 pcb_t *headProcQ(struct list_head *head) {
   	pcb_PTR res = NULL;
-	if (head->next != head) {
-		res=container_of(head->next, pcb_t, p_list);
+	struct list_head *test = list_next(head);
+	if (test) {
+		res=container_of(test, pcb_t, p_list);
 	}
 	return res;
 }
