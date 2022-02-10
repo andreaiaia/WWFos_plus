40ded506feac5b0e0ae81ca4affa42a03b748e05
Wifino <niccolo.cicioni@gmail.com>
2022-02-10 13:55:02 +0100

    Update pcb.c

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -179,7 +179,7 @@ int emptyChild(pcb_t *p) {
 */
 void insertChild(pcb_t *prnt, pcb_t *p) {
 	p->p_parent = prnt;
-	list_add(&(p->p_list), &(prnt->p_child));
+	list_add_tail(&(p->p_list), &(prnt->p_child));
 }
 
 /*
