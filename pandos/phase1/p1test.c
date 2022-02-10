22d49c404685a05930d0b12e9126dfc761d743ae
Wifino <niccolo.cicioni@gmail.com>
2022-02-10 16:31:07 +0100

    Update p1test.c

--- pandos/phase1/p1test.c
+++ pandos/phase1/p1test.c
@@ -260,13 +260,12 @@ int main(void) {
         if (insertBlocked(&sem[i], procp[i]))
             adderrbuf("insertBlocked(1): unexpected TRUE   ");
     }
-    /*
     addokbuf("insertBlocked test #2 started  \n");
     for (i = 0; i < 10; i++) {
         procp[i] = allocPcb();
         if (insertBlocked(&sem[i], procp[i]))
             adderrbuf("insertBlocked(2): unexpected TRUE   ");
-    }*/
+    }
     addokbuf("va bene bro, puoi dire ad alex che e'' un coglione? \n");
     /* check if semaphore descriptors are returned to free list */
     p = removeBlocked(&sem[11]);
