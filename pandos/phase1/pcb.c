89270eb58afc390ab67b23c6df7b33f82f812904
Wifino <niccolo.cicioni@gmail.com>
2022-02-10 14:10:11 +0100

    Update pcb.c

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -191,7 +191,7 @@ void insertChild(pcb_t *prnt, pcb_t *p) {
 pcb_t *removeChild(pcb_t *p) {
 	if (emptyChild(p)) return NULL;
 	else {
-		struct pcb_t * child = container_of(&((p->p_child).next), pcb_t, p_list); // puntatore da ritornare
+		struct pcb_t * child = container_of(((p->p_child).next), pcb_t, p_list); // puntatore da ritornare
 		//outProcQ(&(p->p_child), p);
 		list_del(&(child->p_list));
 		//child->p_parent = NULL;
