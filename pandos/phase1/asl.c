f140893ae0d7d7df8f2c1cb5c035092c8e58ff81
Wifino <niccolo.cicioni@gmail.com>
2022-02-10 16:03:29 +0100

    Update asl.c

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -28,9 +28,9 @@ int insertBlocked(int *semAdd, pcb_t *p) {
     /* scorre la lista dei semafori attivi/utilizzati */
     list_for_each_entry(tmp, &semd_h, s_link){
         if ((tmp->s_key == semAdd) && (flag==0)) {
-            p_semAdd = tmp->s_key;
+            p->p_semAdd = tmp->s_key;
             *(tmp->s_key) = *(tmp->s_key) + 1;
-            list_add_tail(p->p_list, &(tmp->s_procq));
+            list_add_tail(&(p->p_list), &(tmp->s_procq));
             flag=1;
             return FALSE;
         }
