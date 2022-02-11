46ab3804be4ca1b50bfc61aec4f0d7720db8b6b8
Wifino <niccolo.cicioni@gmail.com>
2022-02-11 12:58:24 +0100

    Update asl.c

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -69,7 +69,8 @@ pcb_t *removeBlocked(int *semAdd) {
     list_for_each_entry(s_iter, &semd_h, s_link) {  
         if (s_iter->s_key == semAdd) {
             //pcb_PTR tmp = container_of(&(s_iter.s_procq)->next), pcb_t, p_list);
-            res = removeProcQ(&(s_iter->s_procq));
+            //res = removeProcQ(&(s_iter->s_procq));
+            list_del(&(s_iter->s_procq));
             if (emptyProcQ(&(s_iter->s_procq))){
                  list_del(&(s_iter->s_link));
                  list_add(&(s_iter->s_link), &semdFree_h);
