187bdc9b22cc99e7d9c535b2988041ed1f728d55
Wifino <niccolo.cicioni@gmail.com>
2022-02-11 13:31:31 +0100

    Update asl.c

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -31,7 +31,7 @@ int insertBlocked(int *semAdd, pcb_t *p) {
         if ((tmp->s_key == semAdd) && (flag==0)) {
             p->p_semAdd = tmp->s_key;
             *(tmp->s_key) = *(tmp->s_key) + 1;
-            list_add_tail(&(p->p_list), &(tmp->s_procq));
+            list_add(&(p->p_list), &(tmp->s_procq));
             flag=1;
             return FALSE;
         }
