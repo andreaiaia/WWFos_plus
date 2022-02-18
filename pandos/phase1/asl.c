5567c239c896d70518a62fd92379dcf84d403d1a
Wifino <niccolo.cicioni@gmail.com>
2022-02-18 21:33:30 +0100

    Update asl.c

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -94,54 +94,19 @@ pcb_t *outBlocked(pcb_t *p) {
     semd_PTR sem_iteratore;
     pcb_PTR pcb_iteratore;
     list_for_each_entry(sem_iteratore, &semd_h, s_link){
-        /*list_for_each_entry(pcb_iteratore, &sem_iteratore->s_procq, p_list) {
-            if (p->p_semAdd == sem_iteratore->s_key) { // no passa mai qui
-                addokbuf("trovo il semaforo giusto  \n");
-                list_del(&(pcb_iteratore->p_list));*/
-                outProcQ(&(sem_iteratore->s_procq), p);
-                if (list_empty(&(sem_iteratore->s_procq))) {
-                    sem_iteratore->s_key=NULL;
-                    list_del(&(sem_iteratore->s_link));
-                    list_add(&(sem_iteratore->s_link), &semdFree_h );
-                }
-                addokbuf("returno p  \n");
-                return p;
-            }
-        //}
-    //}
-    // Stato di errore
-    return NULL;
-}
-
-
-
-
-
-/*pcb_t *outBlocked(pcb_t *p) {
-    semd_PTR s_iter = NULL;
-    // Questo ciclo scorre la lista dei semafori (semd_h) e ad ogni iterata s_iter punta al semd_t corrente
-    list_for_each_entry(s_iter, &semd_h, s_link) {
-        addokbuf("itero i semafori  \n");
-        pcb_PTR p_iter = NULL ;
-        list_for_each_entry(p_iter, &(s_iter->s_procq), p_list) {
-            addokbuf("cerco il semaforo  \n");
-            if (p->p_semAdd == s_iter->s_key) {
-                addokbuf("trovo il semaforo giusto  \n");
-                outProcQ(&(s_iter->s_procq), p);
-                if (list_empty(&(s_iter->s_procq))) {
-                    s_iter->s_key=NULL;
-                    list_del(&(s_iter->s_link));
-                    list_add(&(s_iter->s_link), &semdFree_h );
-                }
-                addokbuf("returno p  \n");
-                return p;
-            }
+        outProcQ(&(sem_iteratore->s_procq), p);
+        if (list_empty(&(sem_iteratore->s_procq))) {
+            sem_iteratore->s_key=NULL;
+            list_del(&(sem_iteratore->s_link));
+            list_add(&(sem_iteratore->s_link), &semdFree_h );
         }
-    }
+        addokbuf("returno p  \n");
+        return p;
+    }   
     // Stato di errore
     return NULL;
 }
-*/
+
 /*
     17. 
     Restituisce (senza rimuoverlo) il puntatore al primo PCB della coda dei processi associata al SEMD
@@ -155,6 +120,7 @@ pcb_t *headBlocked(int *semAdd) {
     semd_PTR s_iter;
     list_for_each_entry(s_iter, &semd_h, s_link) {
         if (s_iter->s_key == semAdd) {
+            addokbuf("dio porco p  \n");
             if (list_empty(&(s_iter->s_procq))) return NULL; // La coda dei pcb è vuota
             // Uso la macro container_of per prendere il primo pcb della coda e restituirlo
             //pcb_PTR p = container_of( &(s_iter->s_procq), pcb_t, p_list );
