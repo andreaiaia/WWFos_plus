0df03fd6361da95f84cd3c11d581235c0189eadd
Wifino <niccolo.cicioni@gmail.com>
2022-02-18 12:38:21 +0100

    Update asl.c

--- pandos/phase1/asl.c
+++ pandos/phase1/asl.c
@@ -71,16 +71,11 @@ pcb_t *removeBlocked(int *semAdd) {
         if (s_iter->s_key == semAdd) {
             pcb_PTR tmp = container_of(list_next(&s_iter->s_procq), pcb_t, p_list);
             res = tmp;
-            if (list_empty(&(s_iter->s_procq))) {
-                addokbuf("il container of ritorna null\n");
-            }
             res = removeProcQ(&(s_iter->s_procq)); //risultato diverso chiedere al tutor
-            addokbuf("ANDREA HA STATO QUI\n");
             //list_del(&(tmp->p_list)); //risultato diverso chiedere al tutor
             if (list_empty(&(s_iter->s_procq))) {
                  list_del(&(s_iter->s_link));
                  list_add(&(s_iter->s_link), &semdFree_h);
-                 addokbuf("ANDREA HA STATO pure QUI\n");
             }
             return res;
         }  
