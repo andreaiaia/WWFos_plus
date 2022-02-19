7700ffa68976e309b1c8dc523b73e426887d0471
Andrea Bianchi <bianan96@gmail.com>
2022-02-19 17:08:38 +0100

    fix include p1test

--- pandos/phase1/p1test.c
+++ pandos/phase1/p1test.c
@@ -12,12 +12,12 @@
  *      Modified by Michael Goldweber on May 15, 2004
  */
 
-#include <../h/pandos_const.h>
-#include <../h/pandos_types.h>
+#include "../h/pandos_const.h"
+#include "../h/pandos_types.h"
 
 #include <umps3/umps/libumps.h>
-#include <pcb.h>
-#include <asl.h>
+#include "pcb.h"
+#include "asl.h"
 
 #define MAXPROC	20
 #define	MAXSEM	MAXPROC
