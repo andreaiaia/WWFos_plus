81070e1fa375ee8ca2f248ae4444022dd0f38d9a
Andrea Bianchi <bianan96@gmail.com>
2022-03-26 14:15:22 +0100

    reorganize files

---
+++ pandos/phase2/main.c
@@ -0,0 +1,9 @@
+// Variabili Globali
+/*
+    - Conteggio processi vivi
+    - Conteggio processi bloccati
+    - Coda dei processi "ready"
+    - Puntatore a processo correntemente attivo
+    - Un semaforo (una variabile int) per ogni (sub) dispositivo, non sono tutti sempre attivi insieme
+    - Strutture dati già create in fase 1
+*/
