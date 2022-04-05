98760ddb0bd8d981f2d1dddc5f1bfcafb4366ae6
Wifino <niccolo.cicioni@gmail.com>
2022-04-05 11:07:14 +0200

    Update exception.c

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -1,3 +1,9 @@
+
+// CP0 Cause fields
+// #define CAUSE_EXCCODE_MASK     0x0000007c
+// #define CAUSE_EXCCODE_BIT      2
+// #define CAUSE_GET_EXCCODE(x)   (((x) & CAUSE_EXCCODE_MASK) >> CAUSE_EXCCODE_BIT)
+
 void exceptionHandler() {
 
 
