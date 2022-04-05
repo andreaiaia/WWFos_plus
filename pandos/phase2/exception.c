b140e13c137490e1a9adbd8ff28b744f3f3d3427
Wifino <niccolo.cicioni@gmail.com>
2022-04-05 12:45:54 +0200

    Update exception.c

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -5,23 +5,23 @@
 // #define CAUSE_EXCCODE_BIT      2
 // #define CAUSE_GET_EXCCODE(x)   (((x) & CAUSE_EXCCODE_MASK) >> CAUSE_EXCCODE_BIT)
 // ! L'assunto è che CAUSE_GET_EXCCODE restituisca l'exception code relativo al registro
-// ! cause passato. Quindi gli passo CP0_Cause che è il registro cause della cpu0
+// ! cause passato. Quindi gli passo causa che contiene il registro cause della cpu0 ottenuto da getCause();
 void exceptionHandler() {
     // * Exception code 0 -> Si passa il controllo al device interrupt handler
     // * Exception code 1-3 -> Si passa il controllo al TLB exception handler
     // * Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler
     // * Exception code 8 -> Si passa il controllo al SYSCALL exception handler
-     
-    if (CAUSE_GET_EXCCODE(CP0_Cause) == 0) {
+    memaddr causa = getCause();
+    if (CAUSE_GET_EXCCODE(causa) == 0) {
         interruptHandler();
     } 
-    else if ((CAUSE_GET_EXCCODE(CP0_Cause) >= 1) && (CAUSE_GET_EXCCODE(CP0_Cause) <= 3)) {
+    else if ((CAUSE_GET_EXCCODE(causa) >= 1) && (CAUSE_GET_EXCCODE(causa) <= 3)) {
         uTLB_RefillHandler(); 
     } 
-    else if (((CAUSE_GET_EXCCODE(CP0_Cause) >= 4) && (CAUSE_GET_EXCCODE(CP0_Cause) <= 7)) || ((CAUSE_GET_EXCCODE(CP0_Cause) >= 9) && (CAUSE_GET_EXCCODE(CP0_Cause) <= 12)))  {
+    else if (((CAUSE_GET_EXCCODE(causa) >= 4) && (CAUSE_GET_EXCCODE(causa) <= 7)) || ((CAUSE_GET_EXCCODE(causa) >= 9) && (CAUSE_GET_EXCCODE(causa) <= 12)))  {
         // ! Program trap exception handler
     }
-    else if (CAUSE_GET_EXCCODE(CP0_Cause) == 8) {
+    else if (CAUSE_GET_EXCCODE(causa) == 8) {
         // !SYSCALL EXCEPTION HANDLER
     }
 
