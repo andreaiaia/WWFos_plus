315799ea8ba3b5959b8ce9292209279fde2151f7
Wifino <niccolo.cicioni@gmail.com>
2022-04-05 13:11:41 +0200

    Update exception.c

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -11,17 +11,16 @@ void exceptionHandler() {
     // * Exception code 1-3 -> Si passa il controllo al TLB exception handler
     // * Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler
     // * Exception code 8 -> Si passa il controllo al SYSCALL exception handler
-    memaddr causa = getCause();
-    if (CAUSE_GET_EXCCODE(causa) == 0) {
+    if (CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) == 0) {
         interruptHandler();
     } 
-    else if ((CAUSE_GET_EXCCODE(causa) >= 1) && (CAUSE_GET_EXCCODE(causa) <= 3)) {
+    else if ((CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) >= 1) && (CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) <= 3)) {
         uTLB_RefillHandler(); 
     } 
-    else if (((CAUSE_GET_EXCCODE(causa) >= 4) && (CAUSE_GET_EXCCODE(causa) <= 7)) || ((CAUSE_GET_EXCCODE(causa) >= 9) && (CAUSE_GET_EXCCODE(causa) <= 12)))  {
+    else if (((CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) >= 4) && (CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) <= 7)) || ((CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) >= 9) && (CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) <= 12)))  {
         // ! Program trap exception handler
     }
-    else if (CAUSE_GET_EXCCODE(causa) == 8) {
+    else if (CAUSE_GET_EXCCODE(((state_t *)BIOSDATAPAGE)->cause) == 8) {
         // !SYSCALL EXCEPTION HANDLER
     }
 
