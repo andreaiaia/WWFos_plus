ee17fe78ac54e3e562e8cb24a4c31c60e76116ee
Andrea Bianchi <bianan96@gmail.com>
2022-04-06 21:57:27 +0200

    Move global variables declarations to main.h

--- pandos/phase2/main.c
+++ pandos/phase2/main.c
@@ -1,41 +1,4 @@
-#include "../h/listx.h"
-#include "../h/pandos_types.h"
-#include "../h/pandos_const.h"
-#include "../phase1/asl.h"
-#include "../phase1/pcb.h"
 #include "main.h"
-//#include "scheduler.h"
-//#include "exception.h"
-
-//* Costanti */
-// Numero di semafori dei dispositivi
-#define DEVSEM_NUM 49
-
-//* Variabili Globali */
-
-// Process Count - Contatore processi vivi (started but not yet finished)
-int proc_count;
-// Soft-Block Count - Contatore dei processi avviati ma non ancora terminati (e quindi bloccati)
-int soft_count;
-
-// tail-pointer dei processi in "ready"; alta e bassa priorità
-// (Le code dei processi ready sono dichiarate in scheduler.h)
-extern struct list_head *high_ready_q, *low_ready_q;
-
-// Current Process - Puntatore a pcb in stato "Running" (correntemente attivo)
-pcb_PTR current_p;
-// Device Semaphores - we need 49 sem in total
-//* Ultimo semaforo è il pseudo-clock semaphore
-int device_sem[DEVSEM_NUM];
-// Passup Vector
-passupvector_t *pu_vector;
-
-//* Dichiarazioni di funzioni */
-
-// Funzione fornita dalle specifiche, la riscriveremo nella prossima fase
-extern void uTLB_RefillHandler();
-// La funzione test si trova nel file di test fornito
-extern void test();
 
 //* A LONG TIME AGO, IN A MAIN FUNCTION FAR FAR AWAY */
 int main()
