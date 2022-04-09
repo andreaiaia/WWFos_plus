361d59f904d852fd86c0e6bd1f48e84776a06982
Wifino <niccolo.cicioni@gmail.com>
2022-04-09 19:13:06 +0200

    Update exception.h

--- pandos/phase2/exception.h
+++ pandos/phase2/exception.h
@@ -1,10 +1,10 @@
 #ifndef EXC_H
 #define EXC_H
+#include "main.c"
 #include "sistema.h"
 #include "SYSCALL_helpers.h"
 #include "scheduler.h"
 #include "interrupt.h"
-#include "main.c"
 
 void exceptionHandler();
 
