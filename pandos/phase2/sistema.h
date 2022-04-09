3a47a79caca8bf85bf6c4cdea8e0f359ac84fb03
Wifino <niccolo.cicioni@gmail.com>
2022-04-09 19:05:46 +0200

    try

--- pandos/phase2/sistema.h
+++ pandos/phase2/sistema.h
@@ -1,13 +1,13 @@
 #ifndef SYS_H
     #define SYS_H
-    /*#include <umps3/umps/aout.h>
+    #include <umps3/umps/aout.h>
     #include <umps3/umps/arch.h>
     #include <umps3/umps/bios_defs.h>
     #include <umps3/umps/const.h>
     #include <umps3/umps/cp0.h>
     #include <umps3/umps/libumps.h>
     #include <umps3/umps/regdef.h>
-    #include <umps3/umps/types.h>*/
+    #include <umps3/umps/types.h>
     #include "../h/pandos_types.h"
     #include "../phase1/asl.h"
     #include "../phase1/pcb.h"
