c0887040785f26f5ea48e0c28b98bbcae35c3303
Andrea Bianchi <bianan96@gmail.com>
2022-04-09 18:25:24 +0200

    remove extern usage

--- pandos/phase2/interrupt.c
+++ pandos/phase2/interrupt.c
@@ -31,36 +31,29 @@
   (4.1.4)
   A PLT is the only device attached to the interrupt line 1
 
-  ! COMANDI GIT 
+  ! COMANDI GIT
   git add .
   git commit -m "nome_commit"
   git push
 
-  ? DOMANDE 
+  ? DOMANDE
   ? quali sono i device di tipo "terminal"? inoltre nei terminal bisogna distinguere i 2 casi sub-device
     ? la trasmissione ha priorità su ricezione
   * la linea terminal dovrebbe essere la 7 da quanto scritto su 3.6
 ***************************************************************************************************************************************/
 
-
 #define UNSIGNED_MAX_32_INT 4294967295
-
-extern struct list_head *high_ready_q;
-extern struct list_head *low_ready_q;
-extern int device_sem[DEVSEM_NUM];
-extern pcb_PTR current_p;
-
 
 void interruptHandler()
 {
   // TODO controllare tramite la mask se l'interrupt è lecito
-  for(int line = 1; line < 8; line++) // linea 0 da ignorare
+  for (int line = 1; line < 8; line++) // linea 0 da ignorare
   {
-    if(((state_t*) BIOSDATAPAGE)->cause & CAUSE_IP(line)) 
+    if (((state_t *)BIOSDATAPAGE)->cause & CAUSE_IP(line))
     {
-      if(line == 1)
+      if (line == 1)
         PLTTimerInterrupt(line);
-      else if(line == 2)
+      else if (line == 2)
         intervalTimerInterrupt(line);
       else
         nonTimerInterrupt(line);
@@ -68,23 +61,19 @@ void interruptHandler()
   }
 }
 
-
-
 //* linea 1   (3.6.2 pandos)
 void PLTTimerInterrupt(int line)
 {
   // acknowledgement del PLT interrupt (4.1.4-pops)
-  setTIMER(UNSIGNED_MAX_32_INT);  // ricarico valore 0xFFFF.FFFF
+  setTIMER(UNSIGNED_MAX_32_INT); // ricarico valore 0xFFFF.FFFF
   // ottengo e copio stato processore (che si trova all'indirizzo 0x0FFF.F000, 3.2.2-pops) nel pcb attuale
-  state_t *processor_state = getSTATUS();  // ! secondo me (nick) qui va messo processor_state = ((state_t *)BIOSDATAPAGE)
+  state_t *processor_state = getSTATUS(); // ! secondo me (nick) qui va messo processor_state = ((state_t *)BIOSDATAPAGE)
   current_p->p_s = *processor_state;
   // metto current process in Ready Queue e da "running" lo metto in "ready"
-  insertProcQ(low_ready_q, current_p); 
-  current_p = NULL;  // perché lo scheduler altrimenti continua ad eseguirlo
+  insertProcQ(low_ready_q, current_p);
+  current_p = NULL; // perché lo scheduler altrimenti continua ad eseguirlo
   scheduler();
 }
-
-
 
 //* linea 2   (3.6.3 pandos)
 void intervalTimerInterrupt(int line)
@@ -92,55 +81,54 @@ void intervalTimerInterrupt(int line)
   // acknowledgement dell'interrupt (4.1.3-pops)
   LDIT(PSECOND); // carico Interval Timer con 100millisec
   // sblocco tutti i pcb bloccati nel Pseudo-clock semaphore
-  while(removeBlocked(device_sem[48]));
+  while (removeBlocked(device_sem[48]))
+    ;
   // resetto lo pseudo-clock semaphore a 0
   device_sem[48] = 0;
-  if(current_p)
+  if (current_p)
     LDST(BIOSDATAPAGE);
   else
     scheduler();
 }
 
-
 //* linee 3-7    (3.6.1 pandos)
 void nonTimerInterrupt(int line)
 {
-  int device_num = 0; 
-  devregarea_t *device_regs = (devregarea_t*)RAMBASEADDR; // tutor: nel campo deviceRegs->interrupt_dev trovate la interrupt device bitmap
-  unsigned int bitmap_word = device_regs->interrupt_dev[line-3];
- 
+  int device_num = 0;
+  devregarea_t *device_regs = (devregarea_t *)RAMBASEADDR; // tutor: nel campo deviceRegs->interrupt_dev trovate la interrupt device bitmap
+  unsigned int bitmap_word = device_regs->interrupt_dev[line - 3];
+
   //* 1. calcolare indirizzo del device's device register
   // calcolo il n° del device che ha generato l'interrupt nella line
-  
-  unsigned int mask = 1; 
+
+  unsigned int mask = 1;
   int i = 0;
   int flag = 0;
 
-  while((i < 8) & (flag == 0)) // scorro device
+  while ((i < 8) & (flag == 0)) // scorro device
   {
-    if(bitmap_word & mask) // device con interrupt pending trovato
+    if (bitmap_word & mask) // device con interrupt pending trovato
     {
-      device_num = i;  // salvo n° device
+      device_num = i; // salvo n° device
       flag = 1;
 
-      if(device_num == 7)  // se è un terminale
+      if (device_num == 7) // se è un terminale
       {
         termreg_t *device_ptr = DEV_REG_ADDR(line, device_num);
 
-        if(device_ptr->transm_status == 1)  // terminale ha priorità di trasmissione piu' alta rispetto a ricezione
+        if (device_ptr->transm_status == 1) // terminale ha priorità di trasmissione piu' alta rispetto a ricezione
         {
           // ? al momento non so come usare questa info, per ora ho capito come distinguere la priorità
-        }  
+        }
       }
     }
     mask = mask * 2;
   }
 
-
   // ottengo il device's device register
   dtpreg_t *device_ptr = DEV_REG_ADDR(line, device_num); // pag. 28 manuale pops
   //* 2. salvare lo status code
-  unsigned int device_status = device_ptr->status; 
+  unsigned int device_status = device_ptr->status;
   // 3. acknowledgement dell'interrupt
   device_ptr->command = 0; // TODO: trovare cosa scrivere come acknowledgement, 0 è placeholder
   // 4. Verhogen sul semaforo associato al device (sblocco pcb e metto in ready)
