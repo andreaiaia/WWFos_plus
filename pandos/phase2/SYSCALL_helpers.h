b2d0ccf3425ddc3b7fd0e4cd55f80f11b8dbc019
Andrea Bianchi <bianan96@gmail.com>
2022-04-10 10:56:00 +0200

    Remove unused function

--- pandos/phase2/SYSCALL_helpers.h
+++ pandos/phase2/SYSCALL_helpers.h
@@ -3,9 +3,6 @@
 #include "sistema.h"
 #include "scheduler.h"
 
-// Helpers generici
-void copy_state(state_t *original, state_t *dest);
-
 // Helpers collegati alla Terminate_Process
 void Exterminate(pcb_PTR process);
 pcb_PTR find_process(int pid);
