4b6a4d4a0734a894734f87467cb88eb5b9c333d6
Wifino <niccolo.cicioni@gmail.com>
2022-04-12 23:48:15 +0200

    Update helpers.h

--- pandos/phase2/helpers.h
+++ pandos/phase2/helpers.h
@@ -12,5 +12,7 @@ pcb_PTR find_process(int pid);
 void Exterminate(pcb_PTR);
 
 // Helpers collegati al post syscall event (exception handler)
+
+// Incremento PC e Caricamento BIOSDATAPAGE nel registro della CPU.
 void post_syscall();
 #endif
