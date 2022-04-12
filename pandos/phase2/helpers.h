420ab87668c7fa1e3840f51e5e9d602707cb30e9
Wifino <niccolo.cicioni@gmail.com>
2022-04-12 23:48:36 +0200

    Update helpers.h

--- pandos/phase2/helpers.h
+++ pandos/phase2/helpers.h
@@ -4,14 +4,14 @@
 #include "scheduler.h"
 #include "SYSCALL.h"
 
-// Helpers generici
+// * Helpers generici
 void copy_state(state_t *original, state_t *dest);
 
-// Helpers collegati alla Terminate_Process
+// * Helpers collegati alla Terminate_Process
 pcb_PTR find_process(int pid);
 void Exterminate(pcb_PTR);
 
-// Helpers collegati al post syscall event (exception handler)
+// * Helpers collegati al post syscall event (exception handler)
 
 // Incremento PC e Caricamento BIOSDATAPAGE nel registro della CPU.
 void post_syscall();
