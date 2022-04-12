41b1e86b8212dfa6cfd6b114fee352795f1fe3ec
Wifino <niccolo.cicioni@gmail.com>
2022-04-13 00:35:53 +0200

    Update exception.c

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -1,25 +1,20 @@
-    // * Exception code 0 -> Si passa il controllo al device interrupt handler
-    // * Exception code 1-3 -> Si passa il controllo al TLB exception handler
-    // * Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler
-    // * Exception code 8 -> Si passa il controllo al SYSCALL exception handler
-    
 #include "exception.h"
 
 void exceptionHandler() {
-    if (DECODED_EXCEPTION_CAUSE == 0)
-    {
+    // * Exception code 0 -> Si passa il controllo al device interrupt handler
+    if (DECODED_EXCEPTION_CAUSE == 0) {
         interruptHandler();
     }
-    else if ((DECODED_EXCEPTION_CAUSE >= 1) && (DECODED_EXCEPTION_CAUSE <= 3))
-    {
+    // * Exception code 1-3 -> Si passa il controllo al TLB exception handler (PassUpOrDie PGFAULTEXCEPT)
+    else if ((DECODED_EXCEPTION_CAUSE >= 1) && (DECODED_EXCEPTION_CAUSE <= 3)) {
         PassUpOrDie(PGFAULTEXCEPT);
     }
-    else if (((DECODED_EXCEPTION_CAUSE >= 4) && (DECODED_EXCEPTION_CAUSE <= 7)) || ((DECODED_EXCEPTION_CAUSE >= 9) && (DECODED_EXCEPTION_CAUSE <= 12)))
-    {
+    // * Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler (PassUpOrDie GENERALEXCEPT)
+    else if (((DECODED_EXCEPTION_CAUSE >= 4) && (DECODED_EXCEPTION_CAUSE <= 7)) || ((DECODED_EXCEPTION_CAUSE >= 9) && (DECODED_EXCEPTION_CAUSE <= 12))) {
         PassUpOrDie(GENERALEXCEPT);
     }
-    else if (DECODED_EXCEPTION_CAUSE == 8)
-    {
+    // * Exception code 8 -> Si passa il controllo al SYSCALL exception handler
+    else if (DECODED_EXCEPTION_CAUSE == 8) {
         syscallExceptionHandler(REG_A0_ST);        
     }
 }
