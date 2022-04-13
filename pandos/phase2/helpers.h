efdad1d7b9ef7841f893000fbb989e9d0d8c4065
Wifino <niccolo.cicioni@gmail.com>
2022-04-13 14:42:01 +0200

    RENAME DEFINE

--- pandos/phase2/helpers.h
+++ pandos/phase2/helpers.h
@@ -6,12 +6,12 @@
 
 // ! HANDY DEFINES
 #define INCREMENTO_PC current_p->p_s.pc_epc += WORDLEN
-#define STATO_PROCESSO ((state_t *)BIOSDATAPAGE) // * Macro per il SavedStatus.
-#define DECODED_EXCEPTION_CAUSE CAUSE_GET_EXCCODE(STATO_PROCESSO->cause) //* Macro per l'exception code
-#define REG_A0_SS STATO_PROCESSO->reg_a0 // * Macro per reg_a0 in SavedStatus(BIOSDATAPAGE) che contiene il syscallCode.
-#define REG_A1_SS STATO_PROCESSO->reg_a1 // * Macro per reg_a1 in SavedStatus(BIOSDATAPAGE) che contiene il 1° parametro da passare alla syscall.
-#define REG_A2_SS STATO_PROCESSO->reg_a2 // * Macro per reg_a2 in SavedStatus(BIOSDATAPAGE) che contiene il 2° parametro da passare alla syscall.
-#define REG_A3_SS STATO_PROCESSO->reg_a3 // * Macro per reg_a3 in SavedStatus(BIOSDATAPAGE) che contiene il 3° parametro da passare alla syscall.
+#define PROCESSOR_SAVED_STATE ((state_t *)BIOSDATAPAGE) // * Macro per il SavedStatus.
+#define DECODED_EXCEPTION_CAUSE CAUSE_GET_EXCCODE(PROCESSOR_SAVED_STATE->cause) //* Macro per l'exception code
+#define REG_A0_SS PROCESSOR_SAVED_STATE->reg_a0 // * Macro per reg_a0 in SavedStatus(BIOSDATAPAGE) che contiene il syscallCode.
+#define REG_A1_SS PROCESSOR_SAVED_STATE->reg_a1 // * Macro per reg_a1 in SavedStatus(BIOSDATAPAGE) che contiene il 1° parametro da passare alla syscall.
+#define REG_A2_SS PROCESSOR_SAVED_STATE->reg_a2 // * Macro per reg_a2 in SavedStatus(BIOSDATAPAGE) che contiene il 2° parametro da passare alla syscall.
+#define REG_A3_SS PROCESSOR_SAVED_STATE->reg_a3 // * Macro per reg_a3 in SavedStatus(BIOSDATAPAGE) che contiene il 3° parametro da passare alla syscall.
 
 // ! Helpers generici
 
