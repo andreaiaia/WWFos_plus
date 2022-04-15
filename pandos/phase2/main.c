b7ef29bf76837677b6242fa4751bb856ac5697d4
alexlorenzato <alexlorenzato@gmail.com>
2022-04-15 11:47:51 +0200

    ciao

--- pandos/phase2/main.c
+++ pandos/phase2/main.c
@@ -2,7 +2,7 @@
 #include "exception.h"
 #include "interrupt.h"
 
-extern void klog_print(char*);
+//extern void klog_print(char*);
 
 //* Dichiarazioni di funzioni esterne */
 // Funzione fornita dalle specifiche, la riscriveremo nella prossima fase
