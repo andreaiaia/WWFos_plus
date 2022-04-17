8211192e1412fd7e683d9b838614ea5984707f90
Andrea <bianan96@gmail.com>
2022-04-17 20:24:30 +0200

    pretty format

--- pandos/phase2/initial.h
+++ pandos/phase2/initial.h
@@ -12,13 +12,21 @@
 #include "../phase1/pcb.h"
 
 //* HANDY DEFINES
-#define PROCESSOR_SAVED_STATE ((state_t *)BIOSDATAPAGE) // Macro per il SavedStatus.
+// Numero di semafori dei dispositivi
+#define DEVSEM_NUM 49
+// Macro per il Saved Status
+#define PROCESSOR_SAVED_STATE ((state_t *)BIOSDATAPAGE)
 #define INCREMENTO_PC PROCESSOR_SAVED_STATE->pc_epc += WORDLEN
-#define DECODED_EXCEPTION_CAUSE CAUSE_GET_EXCCODE(PROCESSOR_SAVED_STATE->cause) // Macro per l'exception code
-#define REG_A0_SS PROCESSOR_SAVED_STATE->reg_a0                                 // Macro per reg_a0 in SavedStatus(BIOSDATAPAGE) che contiene il syscallCode.
-#define REG_A1_SS PROCESSOR_SAVED_STATE->reg_a1                                 // Macro per reg_a1 in SavedStatus(BIOSDATAPAGE) che contiene il 1° parametro da passare alla syscall.
-#define REG_A2_SS PROCESSOR_SAVED_STATE->reg_a2                                 // Macro per reg_a2 in SavedStatus(BIOSDATAPAGE) che contiene il 2° parametro da passare alla syscall.
-#define REG_A3_SS PROCESSOR_SAVED_STATE->reg_a3                                 // Macro per reg_a3 in SavedStatus(BIOSDATAPAGE) che contiene il 3° parametro da passare alla syscall.
+// Macro per l'exception code
+#define DECODED_EXCEPTION_CAUSE CAUSE_GET_EXCCODE(PROCESSOR_SAVED_STATE->cause)
+// Macro per reg_a0 in SavedStatus(BIOSDATAPAGE) che contiene il syscallCode.
+#define REG_A0_SS PROCESSOR_SAVED_STATE->reg_a0
+// Macro per reg_a1 in SavedStatus(BIOSDATAPAGE) che contiene il 1° parametro da passare alla syscall.
+#define REG_A1_SS PROCESSOR_SAVED_STATE->reg_a1
+// Macro per reg_a2 in SavedStatus(BIOSDATAPAGE) che contiene il 2° parametro da passare alla syscall.
+#define REG_A2_SS PROCESSOR_SAVED_STATE->reg_a2
+// Macro per reg_a3 in SavedStatus(BIOSDATAPAGE) che contiene il 3° parametro da passare alla syscall.
+#define REG_A3_SS PROCESSOR_SAVED_STATE->reg_a3
 
 //* Dichiarazioni di funzioni esterne */
 // Funzione fornita dalle specifiche, la riscriveremo nella prossima fase
