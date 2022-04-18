87e8dce3ba51638055d3693c94812bb187f5ec83
Andrea <bianan96@gmail.com>
2022-04-18 10:37:18 +0200

    format klog prints

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -2,22 +2,23 @@
 
 void exceptionHandler()
 {
-    klog_print("EXH1\n");
+    klog_print("EXH\n");
     // * Exception code 0 -> Si passa il controllo al device interrupt handler
     if (DECODED_EXCEPTION_CAUSE == 0)
     {
-        klog_print("EXH2\n");
+        klog_print("EXH_INT\n");
         interruptHandler();
     }
     // * Exception code 1-3 -> Si passa il controllo al TLB exception handler (PassUpOrDie PGFAULTEXCEPT)
     else if ((DECODED_EXCEPTION_CAUSE >= 1) && (DECODED_EXCEPTION_CAUSE <= 3))
     {
-        klog_print("EXH3\n");
+        klog_print("EXH_TLB\n");
         PassUpOrDie(PGFAULTEXCEPT);
     }
     // * Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler (PassUpOrDie GENERALEXCEPT)
     else if (((DECODED_EXCEPTION_CAUSE >= 4) && (DECODED_EXCEPTION_CAUSE <= 7)) || ((DECODED_EXCEPTION_CAUSE >= 9) && (DECODED_EXCEPTION_CAUSE <= 12)))
     {
+        klog_print("EXH_TRAP\n");
         klog_print_hex(DECODED_EXCEPTION_CAUSE);
         klog_print("\n");
         PassUpOrDie(GENERALEXCEPT);
@@ -25,7 +26,7 @@ void exceptionHandler()
     // * Exception code 8 -> Si passa il controllo al SYSCALL exception handler
     else if (DECODED_EXCEPTION_CAUSE == 8)
     {
-        klog_print("EXH5\n");
+        klog_print("EXH_SYSCALL\n");
         syscallExceptionHandler(REG_A0_SS);
     }
 }
