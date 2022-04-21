135352a78512dc3845ceb16f0e46aa07d317c555
Wifino <niccolo.cicioni@gmail.com>
2022-04-21 20:36:03 +0200

    Update interrupt.c

--- pandos/phase2/interrupt.c
+++ pandos/phase2/interrupt.c
@@ -48,11 +48,11 @@ void intervalTimerInterrupt(int line)
 {
   klog_print("INT_TIMER_INT\n");
   LDIT(PSECOND); // carico Interval Timer con 100millisec
-  klog_print("INT_TIMER_INT ho caricato timer\n");
+  //klog_print("INT_TIMER_INT ho caricato timer\n");
 
   // sblocco tutti i pcb bloccati nel Pseudo-clock semaphore
   pcb_PTR removed = NULL;
-  klog_print("INT_TIMER_INT ho creato pcb\n");
+  //klog_print("INT_TIMER_INT ho creato pcb\n");
   do
   { 
     removed = removeBlocked(&(device_sem[DEVSEM_NUM - 1]));
