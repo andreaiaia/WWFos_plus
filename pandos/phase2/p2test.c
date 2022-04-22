f3fc7719c7fe43160297313ceee7545dd4b0553f
Wifino <niccolo.cicioni@gmail.com>
2022-04-22 17:38:11 +0200

    Update p2test.c

--- pandos/phase2/p2test.c
+++ pandos/phase2/p2test.c
@@ -603,9 +603,7 @@ void p7() {
     print("error: p7 alive after program trap with no trap vector\n");
     PANIC();
 }
-void placeholder() {
-    
-}
+extern void placeholder();
 
 /* p8root -- test of termination of subtree of processes              */
 /* create a subtree of processes, wait for the leaves to block, signal*/
