6a58a48e7b24d413ab5cb4cd9402028ebc2f519d
Wifino <niccolo.cicioni@gmail.com>
2022-04-23 16:40:57 +0200

    Update p2test.c

--- pandos/phase2/p2test.c
+++ pandos/phase2/p2test.c
@@ -18,7 +18,8 @@
 #include "pandos_const.h"
 #include "pandos_types.h"
 #include <umps3/umps/libumps.h>
-
+#include "initial.h"
+extern void placeholder();
 typedef unsigned int devregtr;
 
 /* hardware constants */
