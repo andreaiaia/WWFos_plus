63def748f6c6b482811c9aa7dceb9a074e2d7b61
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 12:23:24 +0200

    t

--- pandos/phase2/interrupt.c
+++ pandos/phase2/interrupt.c
@@ -2,6 +2,7 @@
 
 void interruptHandler()
 {
+  //klog_print("INT_H - entro\n");
   /**
    * Scorro le linee di Cause.IP alla ricerca di un
    * pending interrupt, non guardo la linea 0 perché
@@ -23,7 +24,9 @@ void interruptHandler()
       }
       else
       {
-        deviceInterrupt(line);
+        //klog_print_hex(line);
+        //klog_print("\n");
+        nonTimerInterrupt(line);
         break;
       }
     }
@@ -32,111 +35,144 @@ void interruptHandler()
 
 void PLTTimerInterrupt(int line)
 {
-    setTIMER(MAX_TIME);
-    copy_state(PROCESSOR_SAVED_STATE, &(current_p->p_s));
-    insertProcQ(&low_ready_q, current_p);
-    scheduler();
+  klog_print("PLT\n");
+  setTIMER(MAX_TIME); // ricarico timer
+  copy_state(PROCESSOR_SAVED_STATE, &(current_p->p_s));
+  insertProcQ(&low_ready_q, current_p);
+  //current_p = NULL;
+  scheduler();
 }
 
-// Interrupt su linea 2 (riservata allo pseudoblock)
+// linea 2   (3.6.3 pandos)
 void intervalTimerInterrupt(int line)
 {
-    LDIT(PSECOND);
-    // sblocco tutti i pcb bloccati nel Pseudo-clock semaphore
-    pcb_PTR removed = NULL;
-    do
-    {
-        removed = removeBlocked(&(device_sem[DEVSEM_NUM - 1]));
-        if (removed != NULL)
-        {
-            soft_count--;
-            if (removed->p_prio == 1)
-                insertProcQ(&high_ready_q, removed);
-            else
-                insertProcQ(&low_ready_q, removed);
-        }
-    } while (removed != NULL);
+  klog_print("INT_TIMER_INT\n");
+  LDIT(PSECOND); 
+  ////klog_print("INT_TIMER_INT ho caricato timer\n");
 
-    // Azzero lo pseudo-clock semaphore
-    device_sem[DEVSEM_NUM - 1] = 0;
-    if (current_p == NULL)
-        scheduler();
-    else
-        postSyscall();
+  // sblocco tutti i pcb bloccati nel Pseudo-clock semaphore
+  pcb_PTR removed = NULL;
+  ////klog_print("INT_TIMER_INT ho creato pcb\n");
+  do
+  { 
+    removed = removeBlocked(&(device_sem[DEVSEM_NUM - 1]));
+    //! qui ci eravamo dimenticati un semaforo a null
+    ////klog_print("INT_TIMER_INT ho rimosso\n");
+    if (removed != NULL)
+    {
+      soft_count--;
+      //klog_print("INT_H: Ho sbloccato un proc\n");
+      if (removed->p_prio == 1)
+        insertProcQ(&high_ready_q, removed);
+      else
+        insertProcQ(&low_ready_q, removed);
+    }
+  } while (removed != NULL);
+
+  // Azzero lo pseudo-clock semaphore
+  device_sem[DEVSEM_NUM - 1] = 0; 
+  if (current_p == NULL)
+  {
+    scheduler();
+  }
+  else
+  {
+    //LDST(PROCESSOR_SAVED_STATE);
+    postSyscall();
+  }
 }
 
-// Linee 3-7, interrupt sui device
-void deviceInterrupt(int line)
+// linee 3-7   (3.6.1 pandos)
+void nonTimerInterrupt(int line)
 {
-    int device_num = 0;
-    devregarea_t *dev_regs = (devregarea_t *)RAMBASEADDR;
-    /**
-     * Salvo la word della line in cui trovo i device con dei pending interrupt.
-     * Nel campo deviceRegs->interrupt_dev c'è la interrupt device bitmap,
-     * una matrice 5 x 8 con tutte le linee di interrupt dedicate ai dispositivi
-     */
-    unsigned int bitmap_word = dev_regs->interrupt_dev[line - 3];
-    // Variabili utilizzate per lo scorrimento della matrice
-    unsigned int dev_status_code = 0, mask = 1;
-    /**
-     * Uso un intero per distinguere quando il terminale sta ricevendo
-     * da quando sta trasmettendo, in modo da trovare il giusto semaforo
-     * * 1 = il terminale riceve
-     * * 0 = il terminale trasmette
-     */
-    int term_is_recv = 0;
+  klog_print("NTI - nonTimerInterrupt\n");
+  int device_num = 0;
+  devregarea_t *dev_regs = (devregarea_t *)RAMBASEADDR;
+  /**
+   * Salvo la word della line in cui trovo i device con dei pending interrupt.
+   * Nel campo deviceRegs->interrupt_dev c'è la interrupt device bitmap,
+   * una matrice 5 x 8 con tutte le linee di interrupt dedicate ai dispositivi
+   */
+  unsigned int bitmap_word = dev_regs->interrupt_dev[line - 3];
+  // Variabili utilizzate per lo scorrimento della matrice
+  unsigned int dev_status_code = 0, mask = 1;
+  /**
+   * Uso un intero per distinguere quando il terminale sta ricevendo
+   * da quando sta trasmettendo, in modo da trovare il giusto semaforo
+   * * 1 = il terminale riceve
+   * * 0 = il terminale trasmette
+   */
+  int term_is_recv = 0;
 
-    // Scorro i dispositivi
-    for (int i = 0; i < N_DEV_PER_IL; i++)
+  // Scorro i dispositivi
+  for (int i = 0; i < N_DEV_PER_IL; i++)
+  {
+    // Check per trovare il device con un pending interrupt
+    if (bitmap_word & mask)
     {
-        // Check per trovare il device con un pending interrupt
-        if (bitmap_word & mask)
+      // Salvo il numero del device trovato
+      device_num = i;
+      // Se il dev è un terminal
+      if (line == 7)
+      {
+        termreg_t *terminal_ptr = (termreg_t *)DEV_REG_ADDR(line, device_num); // calcolo indirizzo terminale
+
+        /**
+         * Controllo prima se c'è un interrupt sulla linea di trasmissione,
+         * in quanto questa ha la priorità sulla ricezione
+         */
+        if (terminal_ptr->transm_status != READY)
         {
-            // Salvo il numero del device trovato
-            device_num = i;
-            // Se il dev è un terminal
-            if (line == 7)
-            {
-                termreg_t *terminal_ptr = (termreg_t *)DEV_REG_ADDR(line, device_num); // calcolo indirizzo terminale
-                /**
-                 * Controllo prima se c'è un interrupt sulla linea di trasmissione,
-                 * in quanto questa ha la priorità sulla ricezione
-                 */
-                if (terminal_ptr->transm_status != READY)
-                {
-                    dev_status_code = terminal_ptr->transm_status;
-                    terminal_ptr->transm_command = ACK;
-                    term_is_recv = 1;
-                }
-                else if (terminal_ptr->recv_status != READY)
-                {
-                    dev_status_code = terminal_ptr->recv_status;
-                    terminal_ptr->recv_command = ACK;
-                    term_is_recv = 0;
-                }
-            }
-            else
-            {
-                dtpreg_t *device_ptr = (dtpreg_t *)DEV_REG_ADDR(line, device_num);
-                dev_status_code = device_ptr->status;
-                device_ptr->command = ACK;
-            }
-            break;
+          dev_status_code = terminal_ptr->transm_status;
+          terminal_ptr->transm_command = ACK;
+          term_is_recv = 1;
+          //klog_print("ACK dato al term0 transm\n");
         }
-        mask *= 2;
+        else if (terminal_ptr->recv_status != READY)
+        {
+          dev_status_code = terminal_ptr->recv_status;
+          terminal_ptr->recv_command = ACK;
+          term_is_recv = 0;
+          //klog_print("ACK dato al term0 rec\n");
+        }
+      }
+      else
+      {
+        dtpreg_t *device_ptr = (dtpreg_t *)DEV_REG_ADDR(line, device_num);
+        dev_status_code = device_ptr->status;
+        device_ptr->command = ACK;
+      }
+      break;
     }
-    // Ora che ho identificato il dispositivo corretto, risalgo al semaforo associato
-    int sem_num = 8 * (line - 3) + (line == 7 ? 2 * device_num : device_num) + term_is_recv;
-    // E faccio una V sul semaforo trovato
-    pcb_PTR tmp = Verhogen(&(device_sem[sem_num]));
-    if (tmp != NULL)
-    {
-        tmp->p_s.reg_v0 = dev_status_code;
-        soft_count--;
+    mask *= 2;
+  }
+  // Ora che ho identificato il dispositivo corretto, risalgo al semaforo associato
+  int sem_num = 8 * (line - 3) + (line == 7 ? 2 * device_num : device_num) + term_is_recv;
+  //klog_print("NTI sem_linea: ");
+  //klog_print_hex(sem_num);
+  //klog_print("\n");
+  pcb_PTR tmp = Verhogen(&(device_sem[sem_num]));
+  if (tmp != NULL){
+    tmp->p_s.reg_v0 = dev_status_code; 
+    soft_count--;
+    } else {
+      klog_print("INT_ TMP NULL");
     }
-    // Termino l'interrupt
-    if (current_p == NULL)
-        scheduler();
-    else
-        postSyscall();
+  // copio stato processore nel pcb attuale
+  //klog_print("NTI copio stato processore\n");
+  if (current_p == NULL)
+  {
+    //klog_print("NTI current_p == NULL\n");
+    scheduler();
+  }
+  else
+  {
+    //klog_print("NTI kariko stato\n");
+    //LDST(PROCESSOR_SAVED_STATE);
+    postSyscall();
+  }
+  // copy_state(PROCESSOR_SAVED_STATE, &(current_p->p_s));
+  // insertProcQ(&low_ready_q, current_p);
+  // scheduler();
+  //  ? come mai non chiamiamo più lo scheduler? fatti i cazzi tuoi
 }
--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -2,45 +2,61 @@
 
 void scheduler()
 {
+    //klog_print("SH - entro\n");
     // Se la coda dei processi ad ALTA priorità è non-vuota
+    //! guarda questo if andrea
     if (!emptyProcQ(&high_ready_q))
+    {
+        //klog_print("SH_HIGH - ce proc in high q\n");
         load_new_proc(&high_ready_q);
+    }
     // Se la coda dei processi a BASSA priorità è non-vuota
     else if (!emptyProcQ(&low_ready_q))
     {
-        if (yielded != NULL)
+        if (yielded != NULL) 
         {
+            //klog_print("SH yielded != NULL\n");
             insertProcQ(&high_ready_q, yielded);
-            yielded = NULL;
+            yielded = NULL; // ! Unico contributo di Alex, signor tutor
         }
+        //klog_print("SH - ce proc in low q\n");
         // Imposto il PLT su 5ms
-        setTIMER(TIMESLICE * *((memaddr *)TIMESCALEADDR));
+        setTIMER(TIMESLICE * *((memaddr *)TIMESCALEADDR)); //!DA RIABILITARE
+        //setTIMER(MAX_TIME);
         load_new_proc(&low_ready_q);
     }
     // Se le code sono entrambe vuote
     else
     {
-        if (proc_count == 0)
+        //klog_print("SH - code vuote\n");
+        //klog_print_hex(proc_count);
+        //klog_print("\n");
+        //klog_print_hex(soft_count);
+        //klog_print("\n");
+        if (proc_count == 0){
+            klog_print("SH: Spengo tutto amici\n");
             HALT(); // Spegne il computer
+        }
         else if (soft_count > 0)
         {
+            //klog_print("SH soft_count > 0\n");
             // Imposto lo stato corrente per accettare interrupt
+            // E disabilito il tutto il resto (quindi anche il PLT)
+            //unsigned int waitingStatus = getSTATUS() | IECON | IEPON | IMON;
             unsigned int waitingStatus = getSTATUS() | IECON | IMON;
-            // Carico il PLT al massimo per evitare PLTTimerInterrupts
+            //klog_print("SH - setState accetto interrupt\n");
             setTIMER(MAX_TIME);
             current_p = NULL;
+            klog_print("SH - vado in Wait\n");
             setSTATUS(waitingStatus);
-            // Entro in WAIT
-            WAIT();
-            /**
-             * Mentre sono in wait continuo a controllare
-             * se ci sono nuovi processi da lanciare
-             */
+            WAIT(); // Aspettando un interrupt
             scheduler();
         }
         else if (soft_count == 0)
         {
+            klog_print("SH - aaaaa panicdeadlock\n");
             // Siamo in deadlock
+            //HALT();
             PANIC();
         }
     }
@@ -54,5 +70,6 @@ void load_new_proc(struct list_head *queue)
     // Faccio partire il timer leggendo il Time of Day
     STCK(start);
     // Carico lo stato del processo sulla CPU
+    //klog_print("LOAD \n");
     LDST(&(current_p->p_s));
 }
