8a01b34eb71bae986051036b0e8e0df78a6ab77d
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 13:48:21 +0200

    t

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -1,75 +1,28 @@
-#include "scheduler.h"
-//qui funziona
-void scheduler()
-{
-    //klog_print("SH - entro\n");
-    // Se la coda dei processi ad ALTA priorità è non-vuota
-    //! guarda questo if andrea
-    if (!emptyProcQ(&high_ready_q))
-    {
-        //klog_print("SH_HIGH - ce proc in high q\n");
-        load_new_proc(&high_ready_q);
-    }
-    // Se la coda dei processi a BASSA priorità è non-vuota
-    else if (!emptyProcQ(&low_ready_q))
-    {
-        if (yielded != NULL) 
-        {
-            //klog_print("SH yielded != NULL\n");
-            insertProcQ(&high_ready_q, yielded);
-            yielded = NULL; // ! Unico contributo di Alex, signor tutor
-        }
-        //klog_print("SH - ce proc in low q\n");
-        // Imposto il PLT su 5ms
-        setTIMER(TIMESLICE * *((memaddr *)TIMESCALEADDR)); //!DA RIABILITARE
-        //setTIMER(MAX_TIME);
-        load_new_proc(&low_ready_q);
-    }
-    // Se le code sono entrambe vuote
-    else
-    {
-        //klog_print("SH - code vuote\n");
-        //klog_print_hex(proc_count);
-        //klog_print("\n");
-        //klog_print_hex(soft_count);
-        //klog_print("\n");
-        if (proc_count == 0){
-            klog_print("SH: Spengo tutto amici\n");
-            HALT(); // Spegne il computer
-        }
-        else if (soft_count > 0)
-        {
-            //klog_print("SH soft_count > 0\n");
-            // Imposto lo stato corrente per accettare interrupt
-            // E disabilito il tutto il resto (quindi anche il PLT)
-            //unsigned int waitingStatus = getSTATUS() | IECON | IEPON | IMON;
-            unsigned int waitingStatus = getSTATUS() | IECON | IMON;
-            //klog_print("SH - setState accetto interrupt\n");
-            setTIMER(MAX_TIME);
-            current_p = NULL;
-            klog_print("SH - vado in Wait\n");
-            setSTATUS(waitingStatus);
-            WAIT(); // Aspettando un interrupt
-            scheduler();
-        }
-        else if (soft_count == 0)
-        {
-            klog_print("SH - aaaaa panicdeadlock\n");
-            // Siamo in deadlock
-            //HALT();
-            PANIC();
-        }
-    }
-}
+#include "exception.h"
 
-// Funzione per prendere un processo dalla coda passata e avviarne l'esecuzione
-void load_new_proc(struct list_head *queue)
+void exceptionHandler()
 {
-    // Prendo il processo da avviare
-    current_p = removeProcQ(queue);
-    // Faccio partire il timer leggendo il Time of Day
-    STCK(start);
-    // Carico lo stato del processo sulla CPU
-    //klog_print("LOAD \n");
-    LDST(&(current_p->p_s));
+    // Fermo il calcolo del tempo usato dal current_p
+    STCK(finish);
+    // Aggiungo il tempo trascorso al tempo impiegato dal processo
+    current_p->p_time = (current_p->p_time) + (finish - start);
+
+    // Exception code 0 -> Si passa il controllo al device interrupt handler
+    if (DECODED_EXCEPTION_CAUSE == 0)
+        interruptHandler();
+    // Exception code 1-3 -> Si passa il controllo al TLB exception handler (PassUpOrDie PGFAULTEXCEPT)
+    else if ((DECODED_EXCEPTION_CAUSE >= 1) && (DECODED_EXCEPTION_CAUSE <= 3))
+    {
+        PassUpOrDie(PGFAULTEXCEPT);
+        scheduler();
+    }
+    // Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler (PassUpOrDie GENERALEXCEPT)
+    else if (((DECODED_EXCEPTION_CAUSE >= 4) && (DECODED_EXCEPTION_CAUSE <= 7)) || ((DECODED_EXCEPTION_CAUSE >= 9) && (DECODED_EXCEPTION_CAUSE <= 12)))
+    {
+        PassUpOrDie(GENERALEXCEPT);
+        scheduler();
+    }
+    // Exception code 8 -> Si passa il controllo al SYSCALL exception handler
+    else if (DECODED_EXCEPTION_CAUSE == 8)
+        syscallExceptionHandler(REG_A0_SS);
 }
