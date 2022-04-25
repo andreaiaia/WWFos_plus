8eb1e2582b858c2011e78dd5c08ec896b7dd8e32
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 12:21:52 +0200

    Update SYSCALL.c

--- pandos/phase2/SYSCALL.c
+++ pandos/phase2/SYSCALL.c
@@ -1,12 +1,14 @@
 #include "SYSCALL.h"
-
+static int compl_soft=0;
 void Create_Process(state_t *statep, int prio, support_t *supportp)
 {
+    //klog_print("SYS_CREATE_PROC - entro\n");
     // Creo il processo figlio
     pcb_PTR child = allocPcb();
     // Se non è stato possibile allocare, ritorno errore
-    if (child == NULL)
+    if (child == NULL) {
         PROCESSOR_SAVED_STATE->reg_v0 = NOPROC; // Ritorno il valore di fallimento
+    }
     else
     {
         // Imposto i campi secondo i parametri ricevuti
@@ -20,13 +22,17 @@ void Create_Process(state_t *statep, int prio, support_t *supportp)
         child->p_pid = (memaddr)child;
         // Associo il process appena creato al suo processo padre
         insertChild(current_p, child);
+
         // Inserisco il processo nella coda corretta
         if (prio)
             insertProcQ(&high_ready_q, child);
         else
+        {
+            klog_print("SYS_CREATE_PROC - insertProcQ\n");
             insertProcQ(&low_ready_q, child);
-
+        }
         // Incremento il conto dei processi
+        //klog_print("SYS_CREATE_PROC - proc_count++\n");
         proc_count++;
         for (int i = 0; i < MAXPROC; i++)
         {
@@ -37,51 +43,85 @@ void Create_Process(state_t *statep, int prio, support_t *supportp)
             }
         }
     }
-    // Restituisco il valore di successo
+    // Ritorno successo
+    //current_p->p_s.reg_v0 = child->p_pid;
+    //klog_print("SYS_CREATE_PROC - fine create_process\n");
     PROCESSOR_SAVED_STATE->reg_v0 = child->p_pid;
 }
 
 // Se il secondo parametro è 0 allora il bersaglio è il processo invocante
 int Terminate_Process(int pid)
 {
-    if (pid == 0)
-    {
+    if (pid == 0) {
         Exterminate(current_p); // Termina il current_p
         current_p = NULL;
-        return 0;
+        return(0);
     }
     else
     {
         pcb_PTR to_terminate = find_process(pid);
         Exterminate(to_terminate); // Termina il proc con il corrispondente pid
-        return 1;
+        return(1);
     }
 }
 
 int Passeren(int *semaddr)
 {
+    //klog_print("PASS - entro\n");
     if (semaddr == NULL)
-        return 1;
+    {
+        //klog_print("PASS1 - sem == NULL\n");
+        return(1);
+    }
     else if (*semaddr == 0)
     {
-        // Blocco il processo corrente
+        //klog_print("PASS2 - sem == 0 (blocco proc)\n");
+        //* Blocco il processo corrente
+        // Aggiungo il processo corrente alla coda del semd
         insertBlocked(semaddr, current_p);
-        return 0;
+        // Incremento il conto dei processi bloccati
+        //soft_count++; // !modifica per manes
+        /**
+         * Rimuovo per sicurezza il processo da qualsiasi
+         * proc_q in cui possa trovarsi
+         */
+        /*if (current_p->p_prio == 1)
+            outProcQ(&high_ready_q, current_p);
+        else
+            outProcQ(&low_ready_q, current_p);*/
+        int flag=1;
+        for (int i=0; i < DEVSEM_NUM; i++) {
+            if (semaddr == &device_sem[i]) {
+                flag=0;
+            } 
+        }
+        if (flag) compl_soft++;
+        return(0);
     }
     else if (headBlocked(semaddr) != NULL)
     {
+        //klog_print("PASS3 - sem != 0 (proc in ready)\n");
         pcb_PTR first = removeBlocked(semaddr);
+        first->p_semAdd = NULL; //! Superflui per modifiche fatte in phase1
+        //soft_count--; // ! modifica per manes
         if (first->p_prio == 1)
             insertProcQ(&high_ready_q, first);
         else
             insertProcQ(&low_ready_q, first);
-
-        return 1;
+                      int flag=1;
+        for (int i=0; i < DEVSEM_NUM; i++) {
+            if (semaddr == &device_sem[i]) {
+                flag=0;
+            } 
+        }
+        if (flag) compl_soft--;
+        return(1);
     }
     else
     {
+        //klog_print("PASS4 - metto sem a 0\n");
         *semaddr = 0;
-        return 1;
+        return(1);
     }
 }
 
@@ -106,7 +146,15 @@ pcb_PTR Verhogen(int *semaddr)
         } else {
             outProcQ(&low_ready_q, current_p);
         }*/
-        
+        //! da qui
+        int flag=1;
+        for (int i=0; i < DEVSEM_NUM; i++) {
+            if (semaddr == &device_sem[i]) {
+                flag=0;
+            } 
+        }
+        if (flag) compl_soft++;
+        // ! a qui, duplicata 4 volte con compl_soft-- in 2 è da togliere.
         current_p = NULL;
 
     }
@@ -120,6 +168,9 @@ pcb_PTR Verhogen(int *semaddr)
                 flag=0;
             } 
         }
+        if (flag) compl_soft--;
+        first->p_semAdd = NULL; //! Superflui per modifiche fatte in phase1
+        //soft_count--; // !spostato nell'interrupt h di alex
         if (first->p_prio == 1)
             insertProcQ(&high_ready_q, first);
         else
@@ -136,6 +187,7 @@ pcb_PTR Verhogen(int *semaddr)
 
 int Do_IO_Device(int *commandAddr, int commandValue)
 {
+    //klog_print("DOIO - entro\n");
     /**
      * Per trovare il dispositivo a cui è associato il
      * commandAddr ricevuto uso una macro che ho definito
@@ -148,16 +200,20 @@ int Do_IO_Device(int *commandAddr, int commandValue)
     else
         dev_position /= 2;
     // Faccio PASSEREN su dispositivo trovato
+    //klog_print("DOIO - faccio passeren: ");
+    //klog_print_hex(dev_position);
+    //klog_print("\n");
     int retValue = Passeren(&(device_sem[dev_position]));
-    //  Scrivo nel commandAddr il valore ricevuto
+    //klog_print("DOIO1 - fatta passeren\n");
+    // Scrivo nel commandAddr il valore ricevuto
     *commandAddr = commandValue;
-    // Restituisco il risultato della Passeren di poco sopra
-    return retValue;
+    return (retValue);
 }
 
 void Get_CPU_Time()
 {
     cpu_t elapsed;
+    // current_p->p_time + tempo trascorso dall'ultimo time slice
     STCK(elapsed);
     // Calcolo e ritorno il tempo trascorso trascorso da quando il processo è attivo
     PROCESSOR_SAVED_STATE->reg_v0 = (unsigned int)(current_p->p_time + (elapsed - start));
@@ -165,7 +221,7 @@ void Get_CPU_Time()
 
 int Wait_For_Clock()
 {
-    return (Passeren(&(device_sem[48])));
+    return(Passeren(&(device_sem[48])));
 }
 
 void Get_Support_Data()
@@ -175,12 +231,18 @@ void Get_Support_Data()
 
 void Get_Process_Id(int parent)
 {
-    if (parent == 0)
+    if (parent == 0) {
         PROCESSOR_SAVED_STATE->reg_v0 = (int)(current_p->p_pid);
-    else if (current_p->p_parent)
-        PROCESSOR_SAVED_STATE->reg_v0 = (int)((current_p->p_parent)->p_pid);
+        //klog_print("SYS_ PID SE STESSO\n");
+    }
     else
-        PROCESSOR_SAVED_STATE->reg_v0 = 0;
+        if (current_p->p_parent){        
+            PROCESSOR_SAVED_STATE->reg_v0 = (int)((current_p->p_parent)->p_pid);
+            //klog_print("SYS_ PID con padre\n");
+        } else {
+            PROCESSOR_SAVED_STATE->reg_v0 = 0;
+            //klog_print("SYS_ SPID = 0 non ha papa\n");
+        }
 }
 
 void Yield()
