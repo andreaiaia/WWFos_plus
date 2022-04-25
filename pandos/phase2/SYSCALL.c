0b3620464e84df12183a1182b1f41562a0f4ef0e
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 13:56:02 +0200

    Update SYSCALL.c

--- pandos/phase2/SYSCALL.c
+++ pandos/phase2/SYSCALL.c
@@ -2,13 +2,11 @@
 
 void Create_Process(state_t *statep, int prio, support_t *supportp)
 {
-    //klog_print("SYS_CREATE_PROC - entro\n");
     // Creo il processo figlio
     pcb_PTR child = allocPcb();
     // Se non è stato possibile allocare, ritorno errore
-    if (child == NULL) {
+    if (child == NULL)
         PROCESSOR_SAVED_STATE->reg_v0 = NOPROC; // Ritorno il valore di fallimento
-    }
     else
     {
         // Imposto i campi secondo i parametri ricevuti
@@ -22,17 +20,13 @@ void Create_Process(state_t *statep, int prio, support_t *supportp)
         child->p_pid = (memaddr)child;
         // Associo il process appena creato al suo processo padre
         insertChild(current_p, child);
-
         // Inserisco il processo nella coda corretta
         if (prio)
             insertProcQ(&high_ready_q, child);
         else
-        {
-            klog_print("SYS_CREATE_PROC - insertProcQ\n");
             insertProcQ(&low_ready_q, child);
-        }
+
         // Incremento il conto dei processi
-        //klog_print("SYS_CREATE_PROC - proc_count++\n");
         proc_count++;
         for (int i = 0; i < MAXPROC; i++)
         {
@@ -43,132 +37,78 @@ void Create_Process(state_t *statep, int prio, support_t *supportp)
             }
         }
     }
-    // Ritorno successo
-    //current_p->p_s.reg_v0 = child->p_pid;
-    //klog_print("SYS_CREATE_PROC - fine create_process\n");
+    // Restituisco il valore di successo
     PROCESSOR_SAVED_STATE->reg_v0 = child->p_pid;
 }
 
 // Se il secondo parametro è 0 allora il bersaglio è il processo invocante
 int Terminate_Process(int pid)
 {
-    if (pid == 0) {
+    if (pid == 0)
+    {
         Exterminate(current_p); // Termina il current_p
         current_p = NULL;
-        return(0);
+        return 0;
     }
     else
     {
         pcb_PTR to_terminate = find_process(pid);
         Exterminate(to_terminate); // Termina il proc con il corrispondente pid
-        return(1);
+        return 1;
     }
 }
 
 int Passeren(int *semaddr)
 {
-    //klog_print("PASS - entro\n");
     if (semaddr == NULL)
-    {
-        //klog_print("PASS1 - sem == NULL\n");
-        return(1);
-    }
+        return 1;
     else if (*semaddr == 0)
     {
-        //klog_print("PASS2 - sem == 0 (blocco proc)\n");
-        //* Blocco il processo corrente
-        // Aggiungo il processo corrente alla coda del semd
+        // Blocco il processo corrente
         insertBlocked(semaddr, current_p);
-        // Incremento il conto dei processi bloccati
-        //soft_count++; // !modifica per manes
-        /**
-         * Rimuovo per sicurezza il processo da qualsiasi
-         * proc_q in cui possa trovarsi
-         */
-        /*if (current_p->p_prio == 1)
-            outProcQ(&high_ready_q, current_p);
-        else
-            outProcQ(&low_ready_q, current_p);*/
-        int flag=1;
-        for (int i=0; i < DEVSEM_NUM; i++) {
-            if (semaddr == &device_sem[i]) {
-                flag=0;
-            } 
-        }
-        return(0);
+        return 0;
     }
     else if (headBlocked(semaddr) != NULL)
     {
-        //klog_print("PASS3 - sem != 0 (proc in ready)\n");
         pcb_PTR first = removeBlocked(semaddr);
-        //soft_count--; // ! modifica per manes
+        first->p_semAdd = NULL;
+
         if (first->p_prio == 1)
             insertProcQ(&high_ready_q, first);
         else
             insertProcQ(&low_ready_q, first);
-                      int flag=1;
-        for (int i=0; i < DEVSEM_NUM; i++) {
-            if (semaddr == &device_sem[i]) {
-                flag=0;
-            } 
-        }
-        return(1);
+
+        return 1;
     }
     else
     {
-        //klog_print("PASS4 - metto sem a 0\n");
         *semaddr = 0;
-        return(1);
+        return 1;
     }
 }
 
-
 pcb_PTR Verhogen(int *semaddr)
 {
-    //klog_print("VER - entro\n");
     pcb_PTR first = NULL;
     if (*semaddr == 1)
     {
-        //klog_print("VER1 - semaddr == 1 (sospendo current_p)\n");
-        // Sospendo il processo corrente
+        //  Sospendo il processo corrente
         copy_state(PROCESSOR_SAVED_STATE, &(current_p->p_s));
-        // Lo inserisco nella coda corretta
-        /*if (current_p->p_prio == 1)
-            insertProcQ(&high_ready_q, current_p);
-        else
-            insertProcQ(&low_ready_q, current_p);
-        */
         insertBlocked(semaddr, current_p);
-        /*if (current_p->p_prio == 1){
-            outProcQ(&high_ready_q, current_p);
-        } else {
-            outProcQ(&low_ready_q, current_p);
-        }*/
         current_p = NULL;
-
     }
     else if (headBlocked(semaddr) != NULL)
     {
-        //klog_print("VER2 - semaddr == 0 (metto in ready)\n");
         first = removeBlocked(semaddr);
-                int flag=1;
-        for (int i=0; i < DEVSEM_NUM; i++) {
-            if (semaddr == &device_sem[i]) {
-                flag=0;
-            } 
-        }
-        
+
         if (first->p_prio == 1)
             insertProcQ(&high_ready_q, first);
         else
             insertProcQ(&low_ready_q, first);
     }
     else
-    {
-        //klog_print("VER3 - metto semaddr a 1\n");
         *semaddr = 1;
-    }
-    //klog_print("VER4 - fine verhogen\n");
+
     return first;
 }
 
