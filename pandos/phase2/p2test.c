1a215e77c396490a9a383f34687a68636a216001
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 12:17:40 +0200

    Update p2test.c

--- pandos/phase2/p2test.c
+++ pandos/phase2/p2test.c
@@ -134,12 +134,15 @@ void uTLB_RefillHandler() {
     LDST((state_t *)0x0FFFF000);
 }
 
-
+void placeholder() {
+    
+}
 /*********************************************************************/
 /*                                                                   */
 /*                 p1 -- the root process                            */
 /*                                                                   */
 void test() {
+    placeholder();
     SYSCALL(VERHOGEN, (int)&sem_testsem, 0, 0); /* V(sem_testsem)   */
 
     print("p1 v(sem_testsem)\n");
