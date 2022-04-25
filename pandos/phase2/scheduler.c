2604a846e4d9289f92b4de35288b4b47e6aab9e5
Wifino <niccolo.cicioni@gmail.com>
2022-04-25 13:48:49 +0200

    ops

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -1,28 +1,58 @@
-#include "exception.h"
+#include "scheduler.h"
 
-void exceptionHandler()
+void scheduler()
 {
-    // Fermo il calcolo del tempo usato dal current_p
-    STCK(finish);
-    // Aggiungo il tempo trascorso al tempo impiegato dal processo
-    current_p->p_time = (current_p->p_time) + (finish - start);
+    // Se la coda dei processi ad ALTA priorità è non-vuota
+    if (!emptyProcQ(&high_ready_q))
+        load_new_proc(&high_ready_q);
+    // Se la coda dei processi a BASSA priorità è non-vuota
+    else if (!emptyProcQ(&low_ready_q))
+    {
+        if (yielded != NULL)
+        {
+            insertProcQ(&high_ready_q, yielded);
+            yielded = NULL;
+        }
+        // Imposto il PLT su 5ms
+        setTIMER(TIMESLICE * *((memaddr *)TIMESCALEADDR));
+        load_new_proc(&low_ready_q);
+    }
+    // Se le code sono entrambe vuote
+    else
+    {
+        if (proc_count == 0)
+            HALT(); // Spegne il computer
+        else if (soft_count > 0)
+        {
+            // Imposto lo stato corrente per accettare interrupt
+            unsigned int waitingStatus = getSTATUS() | IECON | IMON;
+            // Carico il PLT al massimo per evitare PLTTimerInterrupts
+            setTIMER(MAX_TIME);
+            current_p = NULL;
+            setSTATUS(waitingStatus);
+            // Entro in WAIT
+            WAIT();
+            /**
+             * Mentre sono in wait continuo a controllare
+             * se ci sono nuovi processi da lanciare
+             */
+            scheduler();
+        }
+        else if (soft_count == 0)
+        {
+            // Siamo in deadlock
+            PANIC();
+        }
+    }
+}
 
-    // Exception code 0 -> Si passa il controllo al device interrupt handler
-    if (DECODED_EXCEPTION_CAUSE == 0)
-        interruptHandler();
-    // Exception code 1-3 -> Si passa il controllo al TLB exception handler (PassUpOrDie PGFAULTEXCEPT)
-    else if ((DECODED_EXCEPTION_CAUSE >= 1) && (DECODED_EXCEPTION_CAUSE <= 3))
-    {
-        PassUpOrDie(PGFAULTEXCEPT);
-        scheduler();
-    }
-    // Exception code 4-7 9-12 -> Si passa il controllo al Program Trap exception handler (PassUpOrDie GENERALEXCEPT)
-    else if (((DECODED_EXCEPTION_CAUSE >= 4) && (DECODED_EXCEPTION_CAUSE <= 7)) || ((DECODED_EXCEPTION_CAUSE >= 9) && (DECODED_EXCEPTION_CAUSE <= 12)))
-    {
-        PassUpOrDie(GENERALEXCEPT);
-        scheduler();
-    }
-    // Exception code 8 -> Si passa il controllo al SYSCALL exception handler
-    else if (DECODED_EXCEPTION_CAUSE == 8)
-        syscallExceptionHandler(REG_A0_SS);
+// Funzione per prendere un processo dalla coda passata e avviarne l'esecuzione
+void load_new_proc(struct list_head *queue)
+{
+    // Prendo il processo da avviare
+    current_p = removeProcQ(queue);
+    // Faccio partire il timer leggendo il Time of Day
+    STCK(start);
+    // Carico lo stato del processo sulla CPU
+    LDST(&(current_p->p_s));
 }
