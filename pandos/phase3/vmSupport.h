5e6b78b2904b16b169ba258ecbd25c7094f59711
Andrea Bianchi <bianan96@gmail.com>
2022-09-02 21:16:27 +0200

    Add handy defines

--- pandos/phase3/vmSupport.h
+++ pandos/phase3/vmSupport.h
@@ -4,6 +4,12 @@
 #include "pandos_types.h"
 #include "pandos_const.h"
 #include "helpersSupport.h"
+#include "sysSupport.h"
+
+// Renaming some umps const's written in black speech of mordor
+#define TLB_INVALID_LOAD TLBINVLDL
+#define TLB_INVALID_STORE TLBINVLDS
+#define TLB_MODIFICATION 1
 
 void TLB_ExcHandler();
 
