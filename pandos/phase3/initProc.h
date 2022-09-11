cab782f56faf99a41d430e28aea24d78d328e994
Wifino <niccolo.cicioni@gmail.com>
2022-09-11 10:50:02 +0200

    FIXING ANDREA

--- pandos/phase3/initProc.h
+++ pandos/phase3/initProc.h
@@ -1,6 +1,9 @@
 #ifndef INIT_PROC_H
 #define INIT_PROC_H
 
+#include "../h/pandos_types.h"
+#include "../h/pandos_const.h"
+
 // Non sono sicuro del numero di semafori, dal libro
 // (pagina 63) mi sembra di capire che siano 8 flash device,
 // 8 stampanti e 16 terminali, quindi 32 in tutto
