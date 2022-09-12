96c9073f71a034133b71a44dd48f6076839305d7
Andrea Bianchi <bianan96@gmail.com>
2022-09-12 21:29:33 +0200

    prova

--- pandos/phase3/initProc.c
+++ pandos/phase3/initProc.c
@@ -4,7 +4,7 @@
 #include "initProc.h"
 
 // Strutture di supporto dei processi
-support_t support_table[UPROCMAX];
+support_t support_table[72];
 struct list_head support_free;
 
 int mainSemaphore;
