71c7087eb79b1e8f69a700057d02d46313ddf9e7
Andrea Bianchi <bianan96@gmail.com>
2022-09-12 16:22:23 +0200

    Remove device_sem

--- pandos/phase3/initProc.h
+++ pandos/phase3/initProc.h
@@ -6,11 +6,6 @@
 #include <umps3/umps/cp0.h>
 #include "vmSupport.h"
 
-// Non sono sicuro del numero di semafori, dal libro
-// (pagina 63) mi sembra di capire che siano 8 flash device,
-// 8 stampanti e 16 terminali, quindi 32 in tutto
-#define DEVSEM_NUM 32
-
 extern void initSwapStructs();
 
 void test_fase3();
