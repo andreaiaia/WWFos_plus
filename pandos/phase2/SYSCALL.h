8a722bf39f01594dd91c9ccd7824f54fc00f9f37
Andrea Bianchi <bianan96@gmail.com>
2022-09-12 15:47:12 +0200

    moved comments

--- pandos/phase2/SYSCALL.h
+++ pandos/phase2/SYSCALL.h
@@ -17,23 +17,78 @@ extern int proc_count, soft_count, device_sem[DEVSEM_NUM];
 extern pcb_PTR current_p, yielded;
 extern pcb_PTR all_processes[MAXPROC];
 
-// Questa system call crea un nuovo processo come figlio del chiamante.
-void Create_Process(state_t *statep, int prio, support_t *supportp);
 /**
- * Quando invocata, la SYS2 termina il processo indicato
- * dal secondo parametro insieme a tutta la sua progenie.
+ * Questa syscall crea un nuovo processo come progenie,
+ * del chiamante.
+ * @param a1 contiene il puntatore allo stato state_t da
+ * dare come stato iniziale al processo creato.
+ * @param prio specifica in quale ready_queue vada inserito
+ * il processo creato.
+ * @param supportp puntatore alla struttura di supporto da dare
+ * al processo creato.
+ */
+void Create_Process(state_t *statep, int prio, support_t *supportp);
+
+/**
+ * Questa syscall termina il processo indicato dal pid
+ * passato e termina anche tutti i suoi processi figli.
+ * @param pid indica il process id del processo da terminare,
+ * se il parametro è 0 allora il processo da terminare è il chiamante.
  */
 int Terminate_Process(int pid);
-/* SYSCALL sui semafori */
+
+//* SYSCALL sui semafori */
+
+/**
+ * Questa syscall effettua una P sul semaforo indicato.
+ * @param semaddr puntatore al semaforo su cui fare la P.
+ */
 int Passeren(int *semaddr);
+
+/**
+ * Questa syscall effettua una V sul semaforo indicato.
+ * @param semaddr puntatore al semaforo su cui fare la V.
+ */
 pcb_PTR Verhogen(int *semaddr);
-// Syscall sui device
+
+/**
+ * Questa syscall gestisce i servizi di I/O.
+ * @param commandAddr è il puntatore al campo command del
+ * dispositivo da utilizzare per l'operazione I/O.
+ * @param commandValue è il valora da leggere/scrivere usando
+ * il dispositivo.
+ */
 int Do_IO_Device(int *commandAddr, int commandValue);
-// Altre Syscall
+
+/**
+ * Questa syscall restituisce il tempo di processore usato
+ * complessivamente dal processo chiamante.
+ */
 void Get_CPU_Time();
+
+/**
+ * Questa syscall esegue una P sullo pseudo-clock semaphore
+ * del kernel. Questa operazione è sempre bloccante.
+ */
 void Get_Process_Id(int parent);
+
+/**
+ * Questa syscall restituisce un puntatore alla struttura di
+ * supporto del processo corrente.
+ */
 int Wait_For_Clock();
+
+/**
+ * Questa syscall restituisce il pid del processo o del suo genitore.
+ * @param parent se vale 0 la syscall restituisce il pid del processo
+ * chiamante, altrimenti del suo genitore
+ */
 void Get_Support_Data();
+
+/**
+ * Questa syscall sospende il processo corrente,
+ * rimettendono alla fine della sua ready_queue.
+ */
 void Yield();
 
 #endif
