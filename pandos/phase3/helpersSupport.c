d1d0f2fa97e0e55b5211e5c4ea872a1767e39b32
Andrea Bianchi <bianan96@gmail.com>
2022-09-12 20:31:17 +0200

    Simplify TLB exc handler

--- pandos/phase3/helpersSupport.c
+++ pandos/phase3/helpersSupport.c
@@ -4,12 +4,3 @@ int getDeviceSemaphoreIndex(int line, int device, int term_is_recv)
 {
     return 8 * (line - 3) + (line == 7 ? 2 * device : device) + term_is_recv;
 }
-
-size_tt getPTEIndex(memaddr entry_hi)
-{
-    // processo: prendi EntryHI e ricavi il VPN, poi dal VPN ricavi l'index
-    size_tt vpn = entry_hi >> VPNSHIFT;
-    size_tt index = vpn - (KUSEG >> VPNSHIFT);
-
-    return index;
-}
