e3ae43bb65b18ac70c046e2417621e7c93ad7519
Wifino <niccolo.cicioni@gmail.com>
2022-09-15 12:08:58 +0200

    Update exception.c

--- pandos/phase2/exception.c
+++ pandos/phase2/exception.c
@@ -30,9 +30,9 @@ void uTLB_RefillHandler()
     pteEntry_t pte = current_p->p_supportStruct->sup_privatePgTbl[index];
         klog_print("sono nel mezzo della refill bro\n");
 
-    //setENTRYHI(pte.pte_entryHI);
-    current_p->p_supportStruct->sup_privatePgTbl->pte_entryHI = pte.pte_entryHI;
-    TLBP();
+    setENTRYHI(pte.pte_entryHI);
+    //current_p->p_supportStruct->sup_privatePgTbl->pte_entryHI = pte.pte_entryHI;
+    //TLBP();
     if (getINDEX() & PRESENTFLAG){
     // Aggiungo la PTE nel TLB
     klog_print("ho funzionato\n");
